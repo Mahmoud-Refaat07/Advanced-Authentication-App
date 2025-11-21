221eee2a68f408f1b9b4139a67a33ca7e0899c66
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-11-21 23:41:10 +0200

    implemented signup page with password stregnth technique

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  icon: React.ReactNode;
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({ icon, ...props }: Props) => {
+  return (
+    <div className="relative mb-6">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <span className="size-5 text-green-500">{icon}</span>
+      </div>
+      <input
+        {...props}
+        className="w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-500 text-white placeholder-gray-400 transition duration-200"
+      />
+    </div>
+  );
+};
+
+export default Input;
